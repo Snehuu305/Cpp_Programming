69b5f6269b38585beee87b578a38c52b7de84c70
SnehaBorgaon <snehaborgaon26@gmail.com>
2026-07-13 20:49:51 +0530

    Add files via upload

---
+++ arrays/two_pointer/CountBinarySubstring.cpp
@@ -0,0 +1,24 @@
+#include <string>
+using namespace std;
+
+class CountBinarySubstring
+{
+public:
+    int countBinarySubstrings(string s) {
+        int totalCount = 0;
+        int currentRunLength = 1;
+        int previousRunLength = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s[i] == s[i - 1]) {
+                currentRunLength = currentRunLength + 1;
+            } else {
+                previousRunLength = currentRunLength;
+                currentRunLength = 1;
+            }
+            if (previousRunLength != 0) {
+                totalCount += min(previousRunLength, currentRunLength);
+            }
+        }
+        return totalCount;
+    }
+};
