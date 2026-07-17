7d1e43b83b50c744b956b541e3e2997a3850e7f9
SnehaBorgaon <snehaborgaon26@gmail.com>
2026-07-17 22:09:14 +0530

    Refactor mergeAlternately function in MergeAlternately.cpp

--- arrays/two_pointer/MergeAlternately.cpp
+++ arrays/two_pointer/MergeAlternately.cpp
@@ -11,24 +11,19 @@ public:
         int len1 = word1.length();
         int len2 = word2.length();
 
-        while (i < len1 && j < len2)
-        {
+        while (i < len1 && j < len2){
             result += word1[i];
             result += word2[j];
             i++;
             j++;
         }
 
-        // agar word1 lamba hai, uska bacha hua part append kar do
-        while (i < len1)
-        {
+        while (i < len1) {
             result += word1[i];
             i++;
         }
 
-        // agar word2 lamba hai, uska bacha hua part append kar do
-        while (j < len2)
-        {
+        while (j < len2) {
             result += word2[j];
             j++;
         }
