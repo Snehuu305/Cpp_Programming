b99331d3f5e715657601a5ad4c9608e6ee105eee
SnehaBorgaon <snehaborgaon26@gmail.com>
2026-08-11 21:34:11 +0530

    Add files via upload

---
+++ arrays/two_pointer/MaxArea.cpp
@@ -0,0 +1,22 @@
+class MaxArea {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int minHeight = min(height[left], height[right]);
+            int area = width * minHeight;
+
+            maxArea = max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+};
