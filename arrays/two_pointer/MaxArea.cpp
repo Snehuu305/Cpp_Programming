0f851d45106e1acc10d8fdd58ad5cad77ec86a06
SnehaBorgaon <snehaborgaon26@gmail.com>
2026-08-26 23:02:14 +0530

    Reformat MaxArea class and maxArea function
    Refactor maxArea function for improved readability.

--- arrays/two_pointer/MaxArea.cpp
+++ arrays/two_pointer/MaxArea.cpp
@@ -4,16 +4,20 @@ public:
         int left = 0, right = height.size() - 1;
         int maxArea = 0;
 
-        while (left < right) {
+        while (left < right) 
+        {
             int width = right - left;
             int minHeight = min(height[left], height[right]);
             int area = width * minHeight;
 
             maxArea = max(maxArea, area);
 
-            if (height[left] < height[right]) {
+            if (height[left] < height[right])
+            {
                 left++;
-            } else {
+            } 
+            else
+            {
                 right--;
             }
         }
